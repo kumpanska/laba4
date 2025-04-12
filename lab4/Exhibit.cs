9caaecd227ec7c2b1682b53be1be466b91114a7c
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-12 22:49:52 +0300

    add class Exhibit

---
+++ lab4/Exhibit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Exhibit
+    {
+        private AWorkOfArt workOfArt;
+        private Funds funds;
+        private Placement placement;
+        private int costOfExhibit;
+        public Exhibit(AWorkOfArt workOfArt, Funds funds, Placement placement, int costOfExhibit)
+        {
+            this.workOfArt = workOfArt;
+            this.funds = funds;
+            this.placement = placement;
+            this.costOfExhibit = costOfExhibit;
+        }
+        public AWorkOfArt WorkOfArt
+        {
+            get { return workOfArt; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("AWorkOfArt cannot be null.");
+                workOfArt = value;
+            }
+        }
+
+        public Funds Funds
+        {
+            get { return funds; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Fund cannot be null.");
+                funds = value;
+            }
+        }
+        public Placement Placement
+        { get { return placement; }
+            set { placement = value; }
+        }
+
+        public int CostOfExhibit
+        {
+            get { return costOfExhibit; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Cost must be greater than zero.");
+                costOfExhibit = value;
+            }
+        }
+    }
+}
