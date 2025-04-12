e61824c9c0af0b591bd2d67b37f0c8015d9fae6f
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-12 22:05:14 +0300

    add class Funds

---
+++ lab4/Funds.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class Funds
+    {
+        private string nameOfFund;
+        private string addressOfFund;
+        public Funds(string nameOfFund, string addressOfFund)
+        {
+            this.nameOfFund = nameOfFund;
+            this.addressOfFund = addressOfFund;
+        }
+        public string Name
+        {
+            get { return nameOfFund; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Name can't be empty");
+                nameOfFund = value;
+            }
+        }
+        public string Address
+        {
+            get { return addressOfFund; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Address can't be empty");
+                addressOfFund = value;
+            }
+        }
+    }
+}
