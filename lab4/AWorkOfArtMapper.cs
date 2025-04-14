74fb04d0bbc7dda9915f1e0ebcaabf4780094385
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-14 22:53:59 +0300

    add mappers for Funds and DTO Funds, for AWorkOfArt and DTO A WorkOfArt

---
+++ lab4/AWorkOfArtMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public static class AWorkOfArtMapper
+    {
+        public static AWorkOfArt FromDTO(DTOAWorkOfArt dto)
+        {
+            return new AWorkOfArt(
+                dto.NameOfAWorkOfArt,
+                dto.YearOfCreation,
+                dto.Width,
+                dto.Height,
+                dto.Length
+            );
+        }
+        public static DTOAWorkOfArt ToDTO(AWorkOfArt artwork)
+        {
+            return new DTOAWorkOfArt
+            {
+                NameOfAWorkOfArt = artwork.NameOfArt,
+                YearOfCreation = artwork.YearOfCreation,
+                Width = artwork.Width,
+                Height = artwork.Height,
+                Length = artwork.Length
+            };
+        }
+    }
+}
---
+++ lab4/FundsMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public static class FundsMapper
+    {
+        public static Funds FromDTO(DTOFunds dto)
+        {
+            return new Funds(dto.NameOfFund, dto.Address);
+        }
+        public static DTOFunds ToDTO(Funds funds)
+        {
+            return new DTOFunds
+            {
+                NameOfFund = funds.Name,
+                Address = funds.Address
+            };
+        }
+    }
+}
