b86a0a1a8ab2952a63ebc515408cba3bdc323c11
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-14 23:09:51 +0300

    add mapper for exhibit class and dto exhibit class

---
+++ lab4/ExhibitMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public static class ExhibitMapper
+    {
+        public static Exhibit FromDTO(DTOExhibit dto)
+        {
+            return new Exhibit(
+                AWorkOfArtMapper.FromDTO(dto.AWorkOfArt),
+                FundsMapper.FromDTO(dto.Fund),
+                dto.Placement,
+                dto.CostOfExhibit
+            );
+        }
+        public static DTOExhibit ToDTO(Exhibit exhibit)
+        {
+            return new DTOExhibit
+            {
+                AWorkOfArt = AWorkOfArtMapper.ToDTO(exhibit.WorkOfArt), 
+                Fund = FundsMapper.ToDTO(exhibit.Funds),                
+                Placement = exhibit.Placement,                           
+                CostOfExhibit = exhibit.CostOfExhibit
+            };
+        }
+      
+    }
+}
