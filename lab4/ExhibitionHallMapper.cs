7216c57465845f0f2340e38d31877724e5c8085d
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-15 06:53:21 +0300

    add mapper for exhibition hall class and dto class

---
+++ lab4/ExhibitionHallMapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public static class ExhibitionHallMapper
+    {
+        public static ExhibitionHall FromDTO(DTOExhibitionHall dto)
+        {
+            ExhibitionHall hall = new ExhibitionHall(dto.NameOfExhibitionHall);
+
+            foreach (DTOExhibit dtoExhibit in dto.Exhibits)
+            {
+                Exhibit exhibit = ExhibitMapper.FromDTO(dtoExhibit);
+                hall.AddExhibit(exhibit);
+            }
+            return hall;
+        }
+        public static DTOExhibitionHall ToDTO(ExhibitionHall hall)
+        {
+            DTOExhibitionHall dto = new DTOExhibitionHall();
+            dto.NameOfExhibitionHall = hall.NameOfHall;
+            dto.Exhibits = new List<DTOExhibit>();
+            foreach (Exhibit exhibit in hall.Exhibits)
+            {
+                DTOExhibit dtoExhibit = ExhibitMapper.ToDTO(exhibit);
+                dto.Exhibits.Add(dtoExhibit);
+            }
+
+            return dto;
+        }
+    }
+}
