4294b1efb9454940dba733b52e42d5a4494df4dc
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-04-26 21:24:15 +0300

    add methods for save and cancel buttons and messageBox

--- lab4/FundsForm.xaml.cs
+++ lab4/FundsForm.xaml.cs
@@ -19,9 +19,78 @@ namespace lab4
     /// </summary>
     public partial class FundsForm : Window
     {
-        public FundsForm()
+        private string originalFundName;
+        private string originalAddress;
+        private Funds currentFund;
+        private bool isSaved = false;
+        public FundsForm(Funds fund)
         {
             InitializeComponent();
+            currentFund = fund;
+            originalFundName = fund.Name;
+            originalAddress = fund.Address;
+            txtName.Text = fund.Name;
+            txtAddress.Text = fund.Address;
+        }
+        private bool IsDataChanged()
+        {
+            return txtName.Text != originalFundName || txtAddress.Text != originalAddress;
+        }
+        private bool SaveFund()
+        {
+            try
+            {
+                string fundName = txtName.Text;
+                string address = txtAddress.Text;
+                currentFund.Name = fundName;
+                currentFund.Address = address;
+                DTOFunds dto = FundsMapper.ToDTO(currentFund);
+                isSaved = true;
+                return true;
+            }
+            catch (ArgumentException ex)
+            {
+                txtError.Text = ex.Message;
+                return false;  
+            }
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (SaveFund())
+            {
+                DialogResult = true;
+                this.Close();
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!isSaved && IsDataChanged())
+            {
+                MessageBoxResult result = MessageBox.Show(
+                    "Чи зберегти зміни?",
+                    "Збереження",
+                    MessageBoxButton.YesNoCancel,
+                    MessageBoxImage.Question
+                );
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    if (!SaveFund())
+                    {
+                        e.Cancel = true;
+                    }
+                }
+                else if (result == MessageBoxResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
         }
     }
 }
